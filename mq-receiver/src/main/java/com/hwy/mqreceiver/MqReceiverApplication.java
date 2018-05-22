a57be4266461f92a5d539c8d5ad80e6793f0bd93
hweiyu <hwy_dod@163.com>
2018-05-22 09:51:46 +0800

    push resource code

--- mq-receiver/src/main/java/com/hwy/mqreceiver/MqReceiverApplication.java
+++ mq-receiver/src/main/java/com/hwy/mqreceiver/MqReceiverApplication.java
@@ -2,9 +2,7 @@ package com.hwy.mqreceiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.hwy")
 @SpringBootApplication
 public class MqReceiverApplication {
 
---
+++ mq-receiver/src/main/java/com/hwy/mqreceiver/config/MqConfig.java
@@ -0,0 +1,16 @@
+package com.hwy.mqreceiver.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author huangweiyu
+ * @version V1.0
+ * @Title: 描述
+ * @Description: 描述
+ * @date 2018/5/22 9:12
+ **/
+@ComponentScan(basePackages = "com.hwy.mqcomponent.configuration")
+@Configuration
+public class MqConfig {
+}
